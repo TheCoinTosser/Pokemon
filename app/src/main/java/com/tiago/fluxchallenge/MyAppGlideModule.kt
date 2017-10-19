f2029e1de31e1769d547e5d99d753b2fa9516b69
Tiago <tiago16tc@yahoo.com.br>
2017-10-19 13:05:58 +1300

    Added Glide4 dependency

---
+++ app/src/main/java/com/tiago/fluxchallenge/MyAppGlideModule.kt
@@ -0,0 +1,10 @@
+package com.tiago.fluxchallenge
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by tiago on 19/10/17.
+ */
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
